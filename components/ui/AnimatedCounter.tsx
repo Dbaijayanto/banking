d453b732c06238c179112f91a2505b6356447a45
test_repo_0 <baijayanta.das@iic.ac.in>
2025-03-27 02:25:34 +0530

    made home ui (doughnutChart & current balance using chartjs and countup-nodemodule

---
+++ components/ui/AnimatedCounter.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({amount}:{amount:number}) => {
+
+  return (
+    <div classname="w-full">
+        <CountUp
+          duration={2}
+          decimals={2}
+          decimal="."
+          prefix="₹" 
+          end={amount} 
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
