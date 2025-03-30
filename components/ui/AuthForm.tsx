d287e53d6755f3e2a379d24159fa0952ff02303a
deboshree09 <deboshreebarik@gmail.com>
2025-03-30 16:42:21 +0530

    added api key , created database for user signin details and bank details

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -22,6 +22,7 @@ import CustomInput from './CustomInput'
 import { authFromSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.action';
 
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -49,21 +50,18 @@ const AuthForm = ({ type }: { type: string }) => {
         
         if(type === 'sign-up') {
           
-            
-          
+         const newUser = await signUp(data);
 
-          // const newUser = await signUp(userData);
-
-          // setUser(newUser);
+            setUser(newUser);
         }
 
         if(type === 'sign-in') {
-          // const response = await signIn({
-          //   email: data.email,
-          //   password: data.password,
-          // })
+            const response = await signIn({
+             email: data.email,
+             password: data.password,
+            })
 
-          // if(response) router.push('/')
+           if(response) router.push('/')
       
         }
       } catch (error) {
